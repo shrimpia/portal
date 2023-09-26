df14d1cedec7ed306c73f89d6fa4b6c05122c18d
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-09-26 19:29:07 +0900

    enhance(frontend): LinkCardに、ホバー時のエフェクトを追加

--- packages/frontend/src/components/views/LinkCard.tsx
+++ packages/frontend/src/components/views/LinkCard.tsx
@@ -10,13 +10,13 @@ export type LinkCardProp = {
 
 export const LinkCard = (p: LinkCardProp) => {
   return (
-    <Card as={Link} to={p.to} className="border border-0 shadow text-decoration-none">
+    <Card as={Link} to={p.to} className="border border-0 shadow text-decoration-none sh-card-hover">
       <Stack direction="horizontal" className="align-items-center" gap={3}>
         {p.icon && <i className={`${p.icon} fs-2 ms-4`} style={{ color: 'var(--bs-yellow)' }} />}
         <Card.Body>
           <Card.Title>{p.title}</Card.Title>
           {p.children && (
-            <Card.Text className="text-secondary">
+            <Card.Text className="text-muted">
               {p.children}
             </Card.Text>
           )}
