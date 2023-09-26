761cac34d2c17dce96404bb0c2a573b8005da760
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-09-26 18:19:20 +0900

    enhance(frontend): 絵文字の追加申請一覧を、月ごとにまとめるようにレイアウト変更

--- packages/frontend/src/components/domains/emoji-request/RequestsList.tsx
+++ packages/frontend/src/components/domains/emoji-request/RequestsList.tsx
@@ -1,4 +1,6 @@
 import { useAtomValue } from 'jotai';
+import groupBy from 'lodash.groupby';
+import { useMemo, type PropsWithChildren } from 'react';
 import { Card, Col, Container, Image, Row, Stack } from 'react-bootstrap';
 
 
@@ -8,48 +10,55 @@ import { UserLinkView } from '../../views/UserLinkView';
 
 import { StatusBadge } from './StatusBadge';
 
-import type { PropsWithChildren } from 'react';
 
 export type RequestsListProp = PropsWithChildren;
 
 export const RequestsList: React.FC<RequestsListProp> = ({ children }) => {
   const requests = useAtomValue(emojiRequestsAtom);
 
-  return requests.length > 0 ? (
+  const grouped = useMemo(() => groupBy(requests, r => r.createdYear * 100 + r.createdMonth), [requests]);
+  const groupKeys = useMemo(() => Object.keys(grouped).sort((a, b) => Number(b) - Number(a)), [grouped]);
+
+  return groupKeys.length > 0 ? (
     <Container>
-      <Row>
-        {requests.map(r => (
-          <Col key={r.id} xs={12} sm={6} md={4} lg={4} xl={3} className="g-3">
-            <Card>
-              <Card.Body className="overflow-hidden">
-                <Card.Title>:{r.name}:</Card.Title>
-                <Stack direction="vertical" className="align-items-start" gap={3}>
-                  <StatusBadge status={r.status} />
-                  <Image src={r.url} alt={r.name} className="bg-dark rounded p-2" style={{ height: 48 }} />
-                  <div>
-                    <h2 className="fs-6 fw-bold">申請者</h2>
-                    <UserLinkView username={r.username} />
-                  </div>
-                  <div>
-                    <h2 className="fs-6 fw-bold">申請コメント</h2>
-                    {r.comment.trim() ? (
-                      <RichText className="text-muted border-start px-2 mb-0">{r.comment}</RichText>
-                    ) : (
-                      <div className="text-muted">なし</div>
-                    )}
-                  </div>
-                  {r.staffComment && (
-                    <div>
-                      <h2 className="fs-6 fw-bold">スタッフからのコメント</h2>
-                      <RichText className="text-muted border-start px-2 mb-0">{r.staffComment}</RichText>
-                    </div>
-                  )}
-                </Stack>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      {groupKeys.map(key => (
+        <>
+          <Row className="mt-5" key={key}>
+            <h3>{grouped[key][0].createdYear}年{grouped[key][0].createdMonth}月</h3>
+            {grouped[key].map(r => (
+              <Col key={r.id} xs={12} sm={6} md={4} lg={4} xl={3} className="g-3">
+                <Card>
+                  <Card.Body className="overflow-hidden">
+                    <Card.Title>:{r.name}:</Card.Title>
+                    <Stack direction="vertical" className="align-items-start" gap={3}>
+                      <StatusBadge status={r.status} />
+                      <Image src={r.url} alt={r.name} className="bg-dark rounded p-2" style={{ height: 48 }} />
+                      <div>
+                        <h2 className="fs-6 fw-bold">申請者</h2>
+                        <UserLinkView username={r.username} />
+                      </div>
+                      <div>
+                        <h2 className="fs-6 fw-bold">申請コメント</h2>
+                        {r.comment.trim() ? (
+                          <RichText className="text-muted border-start px-2 mb-0">{r.comment}</RichText>
+                        ) : (
+                          <div className="text-muted">なし</div>
+                        )}
+                      </div>
+                      {r.staffComment && (
+                        <div>
+                          <h2 className="fs-6 fw-bold">スタッフからのコメント</h2>
+                          <RichText className="text-muted border-start px-2 mb-0">{r.staffComment}</RichText>
+                        </div>
+                      )}
+                    </Stack>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </>
+      ))}
     </Container>
   ) : children;
 };
