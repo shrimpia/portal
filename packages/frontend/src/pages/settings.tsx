17bdabcdc63f781b68f28dd8a7f0d5c47d23121e
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-10-03 21:46:15 +0900

    chore(frontend): デザイン調整

--- packages/frontend/src/pages/settings.tsx
+++ packages/frontend/src/pages/settings.tsx
@@ -1,13 +1,14 @@
-import { useAtom } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import { useCallback } from 'react';
-import { Container, Form } from 'react-bootstrap';
+import { Card, Container, Form } from 'react-bootstrap';
 
 import type { ThemeType } from '@/types/theme-type';
 
-import { themeAtom } from '@/states/screen';
+import { currentThemeAtom, themeAtom } from '@/states/screen';
 
 const SettingsPage = () => {
   const [theme, setTheme] = useAtom(themeAtom);
+  const currentTheme = useAtomValue(currentThemeAtom);
   const updateTheme = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setTheme(e.target.value as ThemeType);
   }, [setTheme]);
@@ -15,10 +16,15 @@ const SettingsPage = () => {
   return (
     <Container>
       <h1 className="fs-3 mb-5">設定</h1>
-      <h2 className="fs-4">テーマ設定</h2>
-      <Form.Check id="system" type="radio" label="システム設定に合わせる" value="system" checked={theme === 'system'} onChange={updateTheme} />
-      <Form.Check id="dark" type="radio" label="ダークテーマ" value="dark" checked={theme === 'dark'} onChange={updateTheme} />
-      <Form.Check id="light" type="radio" label="ライトテーマ" value="light" checked={theme === 'light'} onChange={updateTheme} />
+      <Card>
+        <Card.Body>
+          <Card.Title><i className={`bi bi-${currentTheme === 'light' ? 'sun' : 'moon'}-fill`} /> テーマ設定</Card.Title>
+          <Card.Text className="text-muted">ポータル全体に適用されるカラーテーマを設定します。</Card.Text>
+          <Form.Check id="system" type="radio" label="システム設定に合わせる" value="system" checked={theme === 'system'} onChange={updateTheme} />
+          <Form.Check id="dark" type="radio" label="ダークテーマ" value="dark" checked={theme === 'dark'} onChange={updateTheme} />
+          <Form.Check id="light" type="radio" label="ライトテーマ" value="light" checked={theme === 'light'} onChange={updateTheme} />
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
