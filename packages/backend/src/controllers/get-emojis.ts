b71172bdc6e0c9a1bcd0e962d125f1c042c7b673
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-10-03 17:32:38 +0900

    fix(dev): wranglerの更新により、ローカル環境でKVが動かない問題を修正

--- packages/backend/src/controllers/get-emojis.ts
+++ packages/backend/src/controllers/get-emojis.ts
@@ -1,8 +1,10 @@
+import { KeyValue } from '../db/repository';
+
 import type { Controller } from './base';
 
 export const getEmojisController: Controller = async (c) => {
-  const data = await c.env.KV.get('emojis');
-  return c.text(data ?? '[]', 200, {
+  const data = await KeyValue.getEmojis(c.env.KV);
+  return c.json(data, 200, {
     'Content-Type': 'application/json',
   });
 };
--- packages/backend/src/db/repository/kv.ts
+++ packages/backend/src/db/repository/kv.ts
@@ -1,7 +1,9 @@
+import type { Emoji } from '../../types/emoji';
+
 export class KeyValueRepository {
   async getEmojis(kv: KVNamespace) {
     const data = await kv.get('emojis');
     if (!data) return [];
-    return JSON.parse(data);
+    return JSON.parse(data) as Emoji[];
   }
 }
