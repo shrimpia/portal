1b593cfa0320a270f96cd573724dbacc6a4560a2
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-12-06 21:16:59 +0900

    wip: イベントカレンダー

---
+++ packages/frontend/src/components/domains/events/EventCardView.tsx
@@ -0,0 +1,41 @@
+import { format } from 'date-fns';
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+import type { EventDto } from '@/types/event';
+
+import { MfmView } from '@/components/common/MfmView';
+import { UserLinkView } from '@/components/common/UserLinkView';
+
+import './EventCardView.scss';
+
+export type EventCardViewProp = {
+    event: EventDto;
+}
+
+const getFormat = (isAllDay: boolean) => isAllDay ? 'yyyy/MM/dd' : 'yyyy/MM/dd HH:mm';
+
+export const EventCardView: React.FC<EventCardViewProp> = ({ event }) => {
+  return (
+    <Card className={`card ${event.isOfficial ? 'official' : ''}`}>
+      <Card.Body>
+        {event.isOfficial ? (
+          <Card.Text className="text-primary fs-small">
+            <i className="bi bi-patch-check-fill" /> 公式
+          </Card.Text>
+        ) : null}
+        <Card.Title>{event.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          {format(new Date(event.startDate), getFormat(event.isAllDay))}
+          {event.endDate && ` ～ ${format(new Date(event.endDate), getFormat(event.isAllDay))}`}
+        </Card.Subtitle>
+        <div>
+          <UserLinkView username={event.authorName} />
+          <div className="mfm">
+            <MfmView>{event.description}</MfmView>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
