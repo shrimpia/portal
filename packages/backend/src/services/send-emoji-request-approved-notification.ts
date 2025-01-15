e2dcee37c6f8f98f1382d4125fa31df176b9988d
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2025-01-15 20:28:57 +0900

    トークンが通知送信に対応していない場合は、絵文字申請処理後のメッセージ送信をスキップするよう変更

--- packages/backend/src/services/send-emoji-request-approved-notification.ts
+++ packages/backend/src/services/send-emoji-request-approved-notification.ts
@@ -6,10 +6,12 @@ import { sendMisskeyNotification } from './send-misskey-notification';
 
 export const sendEmojiRequestApprovedNotification = async (requestId: string, db: D1Database) => {
   const request = await EmojiRequests.readById(db, requestId);
+  // リクエストが存在しない場合は通知を送信しない
   if (!request) return;
 
   const user = await Users.readById(db, request.user_id);
-  if (!user) return;
+  // ユーザーが存在しない、またはトークンバージョンが2未満の場合は通知を送信しない
+  if (!user || user.misskey_token_version < 2) return;
 
   const token = user.misskey_token;
   await sendMisskeyNotification(token, '絵文字の申請が承認されました', `\`:${request.name}:\` → :${request.name}:\n\nうまく表示されないようでしたら、[設定画面](${URL_EMPIRE}/settings/emoji)にてキャッシュをクリアしてみてください。`);
--- packages/backend/src/services/send-emoji-request-rejected-notification.ts
+++ packages/backend/src/services/send-emoji-request-rejected-notification.ts
@@ -4,16 +4,12 @@ import { sendMisskeyNotification } from './send-misskey-notification';
 
 export const sendEmojiRequestRejectedNotification = async (requestId: string, db: D1Database) => {
   const request = await EmojiRequests.readById(db, requestId);
-  if (!request) {
-    console.warn('No such request');
-    return;
-  }
+  // リクエストが存在しない場合は通知を送信しない
+  if (!request) return;
 
   const user = await Users.readById(db, request.user_id);
-  if (!user) {
-    console.warn('No such user');
-    return;
-  }
+  // ユーザーが存在しない、またはトークンバージョンが2未満の場合は通知を送信しない
+  if (!user || user.misskey_token_version < 2) return;
 
   const token = user.misskey_token;
   await sendMisskeyNotification(token, '絵文字の申請が却下されました', `\`:${request.name}:\`\n\n理由: ${request.staff_comment}`);
